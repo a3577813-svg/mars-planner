ab528c4fb76f47843df33ddc65d0445f2052e9af
a3577813-svg <a3577813@gmail.com>
2026-08-03 18:16:22 +0300

    Enable planner attachments on media evidence spread

---
+++ app/book-next6/layout.tsx
@@ -0,0 +1,5 @@
+import PlannerAttachmentEnhancer from "../components/PlannerAttachmentEnhancer";
+
+export default function BookNext6Layout({children}:{children:React.ReactNode}){
+  return <><PlannerAttachmentEnhancer/>{children}</>;
+}
