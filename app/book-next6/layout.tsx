8a77d4cfbc6239191e400defbf4c4de205ce065d
a3577813-svg <a3577813@gmail.com>
2026-08-03 18:20:20 +0300

    Remove image upload from spread 30 until locations are specified

--- app/book-next6/layout.tsx
+++
@@ -1,5 +0,0 @@
-import PlannerAttachmentEnhancer from "../components/PlannerAttachmentEnhancer";
-
-export default function BookNext6Layout({children}:{children:React.ReactNode}){
-  return <><PlannerAttachmentEnhancer/>{children}</>;
-}
