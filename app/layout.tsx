bd5087d1d1957dd7735a229683d5d0df3b896056
a3577813-svg <a3577813@gmail.com>
2026-08-04 07:43:31 +0300

    Enable planner photo and drawing attachments globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./logo-fix.css";
 import CustomCheckboxFields from "./components/CustomCheckboxFields";
+import PlannerAttachmentEnhancer from "./components/PlannerAttachmentEnhancer";
 import LoginRedirect from "./components/LoginRedirect";
 
 export const metadata: Metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body>
         <LoginRedirect />
         <CustomCheckboxFields />
+        <PlannerAttachmentEnhancer />
         {children}
       </body>
     </html>
