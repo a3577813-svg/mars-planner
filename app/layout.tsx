1ab20a601529f3bf495b14424def5425b8736294
a3577813-svg <a3577813@gmail.com>
2026-08-07 22:33:45 +0300

    Временно упростить root layout для диагностики белого экрана

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./logo-fix.css";
-import SeniorStorageNamespace from "./components/SeniorStorageNamespace";
-import CustomCheckboxFields from "./components/CustomCheckboxFields";
-import PlannerAttachmentEnhancer from "./components/PlannerAttachmentEnhancer";
-import SeniorFinalSpreadEnhancer from "./components/SeniorFinalSpreadEnhancer";
-import SeniorSpreadCountEnhancer from "./components/SeniorSpreadCountEnhancer";
-import PlannerSaveStatus from "./components/PlannerSaveStatus";
-import PlannerDesktopEnhancer from "./components/PlannerDesktopEnhancer";
-import DashboardProgressEnhancer from "./components/DashboardProgressEnhancer";
-import DashboardLiveHeader from "./components/DashboardLiveHeader";
-import DashboardCalendar from "./components/DashboardCalendar";
-import DashboardMission from "./components/DashboardMission";
-import DashboardRouteDesign from "./components/DashboardRouteDesign";
-import DashboardFirstPageDesign from "./components/DashboardFirstPageDesign";
-import DashboardNavigationFix from "./components/DashboardNavigationFix";
-import StudentAssignmentEnhancer from "./components/StudentAssignmentEnhancer";
-import TutorSpreadReview from "./components/TutorSpreadReview";
-import AdminCalendarLinkEnhancer from "./components/AdminCalendarLinkEnhancer";
-import AdminSpreadLinkEnhancer from "./components/AdminSpreadLinkEnhancer";
-import AdminSpreadTextEditor from "./components/AdminSpreadTextEditor";
-import LoginRedirect from "./components/LoginRedirect";
-import RoleAccessControl from "./components/RoleAccessControl";
 
 export const metadata: Metadata = {
   title: "Живая планёрка МАРС",
@@ -31,30 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ru">
-      <body>
-        <SeniorStorageNamespace />
-        <LoginRedirect />
-        <RoleAccessControl />
-        <CustomCheckboxFields />
-        <PlannerAttachmentEnhancer />
-        <SeniorSpreadCountEnhancer />
-        <SeniorFinalSpreadEnhancer />
-        <PlannerSaveStatus />
-        <PlannerDesktopEnhancer />
-        <DashboardProgressEnhancer />
-        <DashboardLiveHeader />
-        <DashboardCalendar />
-        <DashboardMission />
-        <DashboardRouteDesign />
-        <DashboardFirstPageDesign />
-        <DashboardNavigationFix />
-        <StudentAssignmentEnhancer />
-        <TutorSpreadReview />
-        <AdminCalendarLinkEnhancer />
-        <AdminSpreadLinkEnhancer />
-        <AdminSpreadTextEditor />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
